fcfa8425a4ab63c7d206e05e6bfe066a5b2e5518
SaraYaffe <syaffe@student.touro.edu>
2022-02-09 19:27:01 -0500

    added ProjectileTest

---
+++ src/test/java/ProjectileTest.java
@@ -0,0 +1,32 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProjectileTest {
+
+    @Test
+    public void getX() {
+        //given
+        Projectile p = new Projectile(52, 17);
+
+        //when
+        double x = p.getX(3); //39.3987
+
+        //then
+        assertEquals(31.3987, x, .0001);
+    }
+
+    @Test
+    public void getY() {
+        //given
+        Projectile p = new Projectile(52, 17);
+
+        //when
+        double y = p.getY(3);
+
+        //then
+        assertEquals(-3.9114, y, .0001);
+    }
+
+
+}
