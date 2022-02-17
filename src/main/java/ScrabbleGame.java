fbd0f24cbb8053823ab5a52fe5e35fa8182006fc
SaraYaffe <syaffe@student.touro.edu>
2022-02-16 20:14:41 -0500

    added ScrabbleGame

---
+++ src/main/java/ScrabbleGame.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ScrabbleGame {
+
+    List<String> playedWords = new ArrayList<>();
+    List<Character> tiles = new ArrayList<>();
+    ScrabbleDictionary dictionary = new ScrabbleDictionary();
+
+    public ScrabbleGame(){
+        //give the player 7 random tiles
+    }
+
+    /**
+     * If the word exists in the ScrabbleDictionary and the letters exist in the tiles List,
+     * remove the letters from the list and add new *random* letters.
+     *
+     * @param word
+     */
+
+    public boolean playWord(String word){
+        return false;
+    }
+}
