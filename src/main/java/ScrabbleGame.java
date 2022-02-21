123d5711ae8e8e5d0eba7260e310c18d06e50650
SaraYaffe <syaffe@student.touro.edu>
2022-02-21 18:40:12 -0500

    modified ScrabbleGame methods and constructor

--- src/main/java/ScrabbleGame.java
+++ src/main/java/ScrabbleGame.java
@@ -5,56 +5,72 @@ public class ScrabbleGame {
     private List<String> playedWords = new ArrayList<>();
     private List<Character> tiles = new ArrayList<>();
     private ScrabbleDictionary dictionary = new ScrabbleDictionary();
-    Random rnd = new Random();
-    Scanner scanner = new Scanner(System.in);
+    private Random rnd = new Random();
 
 
     public ScrabbleGame() {
+        initializeTiles();
+    }
+
+    private void initializeTiles() {
         for (int i = 0; i < 7; i++) {
             tiles.add((char) (rnd.nextInt(26) + 'a'));
         }
-        playGame(scanner);
-
     }
 
     public void playGame(Scanner scanner) {
         while (true) {
-            System.out.println("Enter word with given letters\n" + tiles);
+            System.out.println("Enter word with given letters or 'exit' to end game\n" + tiles);
             String word = scanner.nextLine();
-            char[] wordLetters = word.toCharArray();
-            if (playWord(word, wordLetters)) {
-                playedWords.add(word);
-                for (int i = 0; i < word.length(); i++) {
-                    for (int j = 0; j < tiles.size(); j++) {
-                        if (wordLetters[i] == (tiles.get(j))) {
-                            tiles.remove(tiles.get(j));
-                            tiles.add((char) (rnd.nextInt(26) + 'a'));
-                            //if duplicate in tiles list, only remove one
-                        }
-                    }
-                }
+            if (word.toLowerCase().equals("exit")) {
+                System.exit(0);
+            } else if (!playWord(word)) {
+                System.out.println("invalid word. try again: ");
             }
         }
-
     }
 
-
     /**
-     * If the word exists in the ScrabbleDictionary and the letters exist in the tiles List,
-     * remove the letters from the list and add new *random* letters.
+     * If the word exists in the ScrabbleDictionary, the letters exist in the tiles List,
+     * and playedWords does not yet contain the word,
+     * remove the letters from the list and add new random letters.
      *
      * @param word
      */
 
+    public boolean playWord(String word) {
+        char[] wordLetters = word.toCharArray();
 
-    public boolean playWord(String word, char[] wordLetters) {
-        boolean isTile = false;
-        for (char letter : wordLetters) {
-            isTile = tiles.contains(letter);
-        }
-        if (dictionary.isWord(word) && !playedWords.contains(word) && isTile) {
+        if (dictionary.isWord(word) && !playedWords.contains(word) && isWordInTiles(wordLetters)) {
+            playedWords.add(word);
+            for (int i = 0; i < word.length(); i++) {
+                for (int j = 0; j < tiles.size(); j++) {
+                    if (wordLetters[i] == (tiles.get(j))) {
+                        tiles.remove(tiles.get(j));
+                        tiles.add((char) (rnd.nextInt(26) + 'a'));
+                        j = tiles.size(); //if duplicate in tiles list, only remove one per i
+                    }
+                }
+            }
             return true;
         }
         return false;
+    }
+
+    private boolean isWordInTiles(char[] wordLetters) {
+        List<Character> tilesCopy = new ArrayList<>(tiles);
+
+        for (char letter : wordLetters) {
+            if (tilesCopy.contains(letter))
+                for (int i = 0; i < tilesCopy.size(); i++) {
+                    if (letter == tilesCopy.get(i)) {
+                        tilesCopy.remove(tilesCopy.get(i));
+                        i = tilesCopy.size();//ensures that a word with more than one letter is only valid if contained in tiles
+                    }
+                }
+            else
+                return false;
+        }
+        return true;
     }
 }
